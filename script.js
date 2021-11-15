76c5f9acff714f0bbfe49d1f7e879058f986ce2e
Luke_Hansen <78891567+lukedanuke31@users.noreply.github.com>
2021-11-15 15:04:23 -0600

    big changes

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-//the single-funtionpart
-var button = document.querySelector('.value-btn');
-var button2 = document.querySelector('.value-btn2');
-// add more buttons above this line
-var counter = document.querySelector('.counter');
+var totalClicks = 0;
+var clicks = 0;
 
-button.addEventListener('click', function(){
-  counter.value = parseInt(counter.value) + 1; 
-}, false);
-button2.addEventListener('click', function(){
-  counter.value = parseInt(counter.value) - 1; 
-}, false)
-// cashin area
-var cashin = document.querySelector('.submit-clear');
-var total = document.querySelector('.total-ammount');
+function updateClickCount() {
+  document.getElementById("clickCount").innerHTML = clicks;
+}
 
-cashin.addEventListener('click', function(){
-  total.value = parseInt(counter.value); 
-  counter.value = counter.value - counter.value;
-}, true)
-//total
+function updateTotal(){
+  totalClicks = totalClicks + clicks;
+  document.getElementById("total").innerHTML = totalClicks;
+  clicks = 0;
+  document.getElementById("clickCount").innerHTML = clicks;
+}
